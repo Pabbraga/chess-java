bd6cdf15b3d9aad2863266846225d97cdb494802
Pabbraga <pablofelix567@gmail.com>
2024-02-14 18:55:52 -0300

    feat: add printing possibles moves

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -20,7 +20,7 @@ public class Rook extends ChessPiece {
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
-        Position p = new Position(0,0);
+        Position p = new Position(0, 0);
 
         // above
         p.setValues(position.getRow() - 1, position.getColumn());
@@ -33,10 +33,10 @@ public class Rook extends ChessPiece {
         }
 
         // left
-        p.setValues(position.getRow(), position.getColumn() + 1);
+        p.setValues(position.getRow(), position.getColumn() - 1);
         while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
-            p.setRow(p.getColumn() + 1);
+            p.setColumn(p.getColumn() - 1);
         }
         if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
@@ -46,7 +46,7 @@ public class Rook extends ChessPiece {
         p.setValues(position.getRow(), position.getColumn() + 1);
         while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
-            p.setRow(p.getColumn() + 1);
+            p.setColumn(p.getColumn() + 1);
         }
         if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
